42a0e9bccbbf9a8ed480765d626a94d4d1b8cb2e
Lucia Rigoni Freire <luciarigoni@live.com>
2023-03-20 17:10:11 -0300

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Lucia Rigoni Freire on 20/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
